d2948ed3171ce77b26a3baacfcc228c7d9aeff6b
liyl <295697141@qq.com>
2017-01-15 02:27:22 +0800

    add multiple-webpack-examples

---
+++ multiple-webpack-example/pages/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-new */
+/* eslint-disable no-unused-vars */
+ 
+var Vue = require('vue')
+var App = require('./App.vue')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
---
+++ multiple-webpack-example/pages/subpage/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-new */
+/* eslint-disable no-unused-vars */
+
+var Vue = require('vue')
+var App = require('./App.vue')
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
